243aeeb43b333901f76755e198ed5f12f0c6036b
Pablo Cattaneo <pablocattaneo1@gmail.com>
2019-09-05 08:32:42 -0300

    #master first validation and some users

---
+++ db/validation.js
@@ -0,0 +1,40 @@
+db.createCollection("users", {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      required: ["username", "name", "lastName", "email"],
+      properties: {
+        username: {
+          bsonType: "string",
+          description: "Must be a string and is required"
+        },
+        name: {
+          bsonType: "string",
+          description: "Must be a string and is required"
+        },
+        lastName: {
+          bsonType: "string",
+          description: "Must be a string and is required"
+        },
+        favoritesRecipes: {
+          bsonType: "array",
+          description: "Must be an array",
+          items: {
+            bsonType: "object",
+            required: ["title", "description"],
+            properties: {
+              title: {
+                bsonType: "string",
+                description: "Must be a string and is required"
+              },
+              description: {
+                bsonType: "string",
+                description: "Must be a string and is required"
+              },
+            }
+          }
+        }
+      }
+    }
+  }
+})
