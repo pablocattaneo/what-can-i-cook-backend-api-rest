a1a4129c9948bca831582e9fc1985f8c88b917c7
Pablo Cattaneo <pablocattaneo1@gmail.com>
2019-09-16 07:49:24 -0300

    #master improvements

---
+++ db/products.js
@@ -0,0 +1,73 @@
+[{
+    _id: ObjectId("5d7f5d8283780364cfd3ad8e"),
+    name: "Leche",
+    brand: "Sancor",
+    price: NumberDecimal("60.00"),
+    stock: NumberInt("50"),
+    amount: NumberDecimal("1"),
+    measure: "lt",
+    description: "Leche Sancor",
+    mainImg: ["https://statics.dinoonline.com.ar/imagenes/full_600x600_ma/3261777_f.jpg"],
+    thumbnailImg: "https://statics.dinoonline.com.ar/imagenes/full_600x600_ma/3261777_f.jpg"
+  },
+  {
+    _id: ObjectId("5d7f5d8283780364cfd3ad8f"),
+    name: "Arroz doble carolina",
+    brand: "Gallo",
+    price: NumberDecimal("35.75"),
+    stock: NumberInt("150"),
+    amount: NumberDecimal("500"),
+    measure: "gr",
+    description: "arroz doble carolina Gallo",
+    mainImg: ["https://walmartar.vteximg.com.br/arquivos/ids/843539-1000-1000/Arroz-Doble-Carolina-500gr-1-13412.jpg?v=636861213321700000"],
+    thumbnailImg: "https://walmartar.vteximg.com.br/arquivos/ids/843539-1000-1000/Arroz-Doble-Carolina-500gr-1-13412.jpg?v=636861213321700000"
+  },
+  {
+    _id: ObjectId("5d7f5d8283780364cfd3ad90"),
+    name: "Limón",
+    brand: "Del limonero",
+    price: NumberDecimal("5.00"),
+    stock: NumberInt("150"),
+    amount: NumberDecimal("250"),
+    measure: "gr",
+    description: "Limón",
+    mainImg: ["https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Lemon-Whole-Split.jpg/1920px-Lemon-Whole-Split.jpg"],
+    thumbnailImg: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Lemon-Whole-Split.jpg/1920px-Lemon-Whole-Split.jpg"
+  },
+  {
+    _id: ObjectId("5d7f5d8283780364cfd3ad91"),
+    name: "Azucar",
+    brand: "Ledezma",
+    price: NumberDecimal("15.99"),
+    stock: NumberInt("500"),
+    amount: NumberDecimal("250"),
+    measure: "gr",
+    description: "Azucar Ledezma",
+    mainImg: ["http://www.laizquierdadiario.com/IMG/arton128549.jpg"],
+    thumbnailImg: "http://www.laizquierdadiario.com/IMG/arton128549.jpg"
+  },
+  {
+    _id: ObjectId("5d7f5d8283780364cfd3ad92"),
+    name: "Esencia de vainilla",
+    brand: "Alicante",
+    price: NumberDecimal("25.83"),
+    stock: NumberInt("500"),
+    amount: NumberDecimal("100"),
+    measure: "ml",
+    description: "Esencia de vainilla Alicante",
+    mainImg: ["https://walmartar.vteximg.com.br/arquivos/ids/828997-1000-1000/Esencia-De-Vainilla-Alicante-100-Cc-1-13571.jpg?v=636685099444100000"],
+    thumbnailImg: "https://walmartar.vteximg.com.br/arquivos/ids/828997-1000-1000/Esencia-De-Vainilla-Alicante-100-Cc-1-13571.jpg?v=636685099444100000"
+  },
+  {
+    _id: ObjectId("5d7f5d8283780364cfd3ad93"),
+    name: "Dulce de Leche",
+    brand: "La Serenisima",
+    price: NumberDecimal("35.50"),
+    stock: NumberInt("500"),
+    amount: NumberDecimal("400"),
+    measure: "ml",
+    description: "Dulce de Leche Alicante",
+    mainImg: ["https://cdn11.bigcommerce.com/s-3stx4pub31/images/stencil/608x608/products/277/766/Colonial_400__90823.1556733293.jpg?c=2"],
+    thumbnailImg: "https://cdn11.bigcommerce.com/s-3stx4pub31/images/stencil/608x608/products/277/766/Colonial_400__90823.1556733293.jpg?c=2"
+  }
+]
---
+++ db/recipes.js
@@ -0,0 +1,33 @@
+[{
+    title: "Arroz con leche",
+    description: "El arroz con leche es un postre típico de la gastronomía de múltiples países hecho cociendo lentamente arroz con leche y azúcar. Se sirve frío o caliente. Se le suele espolvorear canela, vainilla o cáscara de limón para aromatizarlo",
+    mainImg: ["https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Arroz_con_leche.png/245px-Arroz_con_leche.png"],
+    thumbnailImg: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Arroz_con_leche.png/245px-Arroz_con_leche.png",
+    ingredient: [{
+        productId: ObjectId("5d7f5d8283780364cfd3ad8e"),
+        amount: NumberDecimal("1")
+      },
+      {
+        productId: ObjectId("5d7f5d8283780364cfd3ad8e"),
+        amount: NumberDecimal("1")
+      },
+      {
+        productId: ObjectId("5d7f5d8283780364cfd3ad90"),
+        amount: NumberDecimal("1")
+      },
+      {
+        productId: ObjectId("5d7f5d8283780364cfd3ad92"),
+        amount: NumberDecimal("1")
+      },
+      {
+        productId: ObjectId("5d7f5d8283780364cfd3ad93"),
+        amount: NumberDecimal("1")
+      }
+    ],
+    comments: {},
+    serving: NumberInt("6"),
+    cookTime: NumberInt("60"),
+    readyIn: {},
+    calories: {}
+  }
+}
