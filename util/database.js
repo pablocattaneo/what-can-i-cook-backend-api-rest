a13ba365b1562331bdbc2c67632b8addd9ec07b7
Pablo Cattaneo <pablocattaneo1@gmail.com>
2019-09-30 09:00:42 -0300

    #master improve code

--- util/database.js
+++ util/database.js
@@ -2,25 +2,20 @@ const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
 function mongoConnect(url, dbName) {
-  const client = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true });
-  
-  // Use connect method to connect to the Server
-  client.connect((err) => {
-    assert.equal(null, err);
+  MongoClient.connect(url)
+  .then(client => {
     console.log("Connected successfully to on mongo server");
-  
     const db = client.db(dbName);
-
     db.collection('products')
       .find()
       .toArray()
       .then(r=>{
         console.log("products", r);
       });
-
-    client.close();
-    
-  }, { useNewUrlParser: true });
+  })
+  .catch(e => {
+    console.log("error", e);
+  });
 }
 
 module.exports = mongoConnect;
