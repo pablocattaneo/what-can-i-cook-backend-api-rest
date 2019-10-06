895366d32fdba12f502e5544012662a22792c02f
Pablo Cattaneo <pablocattaneo1@gmail.com>
2019-10-06 16:00:09 -0300

    #master create controllers folder and home controller

--- routes/home.js
+++ routes/home.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/',(req, res, next) => {
-  res.send('<h1>Welcome to the home!!</h1>');
-})
+const homeController = require('../controllers/home')
+
+router.get('/', homeController.getHome)
 
 module.exports = router;
