935fc33d433493c94d12c78f83d057a06bfa36d7
Pablo Cattaneo <pablocattaneo1@gmail.com>
2019-10-07 06:16:52 -0300

    #master add products controller

--- routes/products.js
+++ routes/products.js
@@ -2,24 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 
-const getDb = require('../util/database').getDb;
+const productsController = require('../controllers/products')
 
-function getProducts() {
-  const db = getDb().db();
-  db.collection('products')
-    .find()
-    .toArray()
-    .then(r=>{
-      console.log("products", r);
-    });
-}
-
-router.get('/products/:productId',(req, res, next) => {
-  res.send(`<h1>Hello products ${ req.params.productId } !!</h1>`);
-})
-router.get('/products/',(req, res, next) => {
-  getProducts();
-  res.send(`<h1>Hello products Home !!</h1>`);
-})
+router.get('/products/:productId', productsController.getProductById)
+router.get('/products/', productsController.getAllProducts)
 
 module.exports = router;
