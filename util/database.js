d4a52a1784f11be1a314df591c9906c222ecb1a3
Pablo Cattaneo <pablocattaneo1@gmail.com>
2019-10-08 20:56:32 -0300

    #master fixlint on db.js

--- util/database.js
+++ util/database.js
@@ -4,24 +4,23 @@ let _db;
 
 function mongoConnect(uri, callback) {
   mongoClient.connect(uri)
-  .then(mongoClientIntance => {
-    console.log("Connected successfully to on mongo server");
-    _db = mongoClientIntance;
-    callback();
-  })
-  .catch(error => {
-    console.log("error", error);
-    throw error
-  });
-};
+    .then((mongoClientIntance) => {
+      console.log('Connected successfully to on mongo server');
+      _db = mongoClientIntance;
+      callback();
+    })
+    .catch((error) => {
+      console.log('error', error);
+      throw error;
+    });
+}
 
 function getDb() {
   if (_db) {
     return _db;
   }
-  throw "No database found";
+  throw 'No database found';
 }
 
 exports.mongoConnect = mongoConnect;
 exports.getDb = getDb;
-
