e962240a44b8801a1b2dd2c774fb03a11528ee65
Pablo Cattaneo <pablocattaneo1@gmail.com>
2020-06-16 09:09:45 -0300

    #master improve jwt security

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -6,7 +6,7 @@ module.exports = (req, res, next) => {
     throw new Error("Not authenticated.");
   }
   const token = authHeader.split(" ")[1];
-  const decodeToken = jwt.verify(token, "somesupersecretsecret");
+  const decodeToken = jwt.verify(token, "P2bQM9L2r4b$}3<X)_cU_+F7-:}'Y-]e(gPH::?^*YW,x5<3*Zrfy=zZ^.K!aJ,6!S&UJ;fTCmnz}.>\,^mtK8d{jw8a88z`yARW@b78K8+TxNg6{Eg?wPXZ3%:fjZY3;V[dX#Y7t\K9]sXXXud+mFjqtM#[q\(UL#.c-L@M99wfJ2RsaSj\Q7x/Gwnmnk+c6-Jb.n-&:J'jzS:fGDa*e6aw;W(u$!R6~cbed}tWw7.[g]yTzx@56Hhz}2{L`6%;");
   if (!decodeToken) {
     throw new Error("Not authenticated");
   }
