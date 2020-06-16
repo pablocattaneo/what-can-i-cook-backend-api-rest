40ab7c0bd0409a9dfb0a6552109dbc0e547187d1
Pablo Cattaneo <pablocattaneo1@gmail.com>
2020-06-16 09:26:51 -0300

    #master disabled prettier rules for the jwt signature

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-escape */
 const { validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
@@ -73,6 +74,7 @@ exports.login = async (req, res, next) => {
         // eslint-disable-next-line no-underscore-dangle
         userId: loadedUser._id.toString()
       },
+      // eslint-disable-next-line prettier/prettier
       "P2bQM9L2r4b$}3<X)_cU_+F7-:}'Y-]e(gPH::?^*YW,x5<3*Zrfy=zZ^.K!aJ,6!S&UJ;fTCmnz}.>\,^mtK8d{jw8a88z`yARW@b78K8+TxNg6{Eg?wPXZ3%:fjZY3;V[dX#Y7t\K9]sXXXud+mFjqtM#[q\(UL#.c-L@M99wfJ2RsaSj\Q7x/Gwnmnk+c6-Jb.n-&:J'jzS:fGDa*e6aw;W(u$!R6~cbed}tWw7.[g]yTzx@56Hhz}2{L`6%;",
       { expiresIn: "1hr" }
     );
