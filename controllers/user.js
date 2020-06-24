3a374cc883a0a91fc28f407f8481d3ae04467c51
Pablo Cattaneo <pablocattaneo1@gmail.com>
2020-06-24 08:01:19 -0300

    #master improve code legibility

--- controllers/user.js
+++ controllers/user.js
@@ -4,8 +4,8 @@ const { getDb } = require("../util/database");
 exports.getUserById = async (req, res) => {
   const { userId } = req.params;
   try {
-    const database = getDb().db();
-    const db = await database;
+    const dbConnection = getDb().db();
+    const db = await dbConnection;
     const usersCollection = await db.collection("users");
     const user = await usersCollection.findOne({ _id: new ObjectId(userId) });
     res.json(user);
