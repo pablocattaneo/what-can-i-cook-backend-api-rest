15e515ee39934458b515cebc10bc77fac8bbf6f4
Pablo Cattaneo <pablocattaneo1@gmail.com>
2020-06-26 10:56:56 -0300

    #master add projection to exclude password in the respond

--- controllers/user.js
+++ controllers/user.js
@@ -7,7 +7,11 @@ exports.getUserById = async (req, res) => {
     const dbConnection = getDb().db();
     const db = await dbConnection;
     const usersCollection = await db.collection("users");
-    const user = await usersCollection.findOne({ _id: new ObjectId(userId) });
+    const user = await usersCollection.findOne(
+      { _id: new ObjectId(userId) },
+      { projection: { password: 0 } }
+    );
+    console.log("user", user);
     res.json(user);
   } catch (error) {
     throw new Error(error);
