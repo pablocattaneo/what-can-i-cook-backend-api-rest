21f0608f333a5ebc6d7388d9d15e176a1b2ee3a5
Pablo Cattaneo <pablocattaneo1@gmail.com>
2020-08-12 09:51:28 -0300

    #7 set extend

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,8 @@ module.exports = {
     node: true,
   },
   extends: [
-    'airbnb-base',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
