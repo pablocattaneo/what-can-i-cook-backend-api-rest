7ef9c2e847ba8e944a8546c5fb8a60d1b41a0795
Pablo Cattaneo <pablocattaneo1@gmail.com>
2020-08-21 10:55:51 -0300

    #7 fix lint

--- src/controllers/recipes.ts
+++ src/controllers/recipes.ts
@@ -179,7 +179,7 @@ export async function getRecipes(req: any, res: Response) {
   }
 }
 
-export function createRecipe(req: Request, res: Response, next: NextFunction) {
+export function createRecipe(req: Request, res: Response, next: NextFunction):void {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     res.status(422).json({
